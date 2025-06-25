667dc725ce6ddf61044b9184368caf1f960297c2
Mickael Contreras <mickaelcontreras508@gmail.com>
2025-06-24 18:05:52 -0700

    refactor: switch to react-native-maps for proper native map usage
    Replaced web-based library with react-native-maps to ensure compatibility with React Native. Removed hardcoded API key for now since deployment is not planned yet.

--- mobileFrontend/App.js
+++ mobileFrontend/App.js
@@ -1,11 +1,27 @@
+import React from 'react';
+import MapView, {PROVIDER_GOOGLE } from 'react-native-maps';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, } from 'react-native';
+
+// import {APIProvider, Map} from '@vis.gl/react-google-maps' // react web library
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>LETS GOOOO!!!!</Text>
       <StatusBar style="auto" />
+        <MapView
+        provider={PROVIDER_GOOGLE} /* This property works on both Android and iOS.
+Rebuild the app binary (or re-submit to the Google */
+          style={styles.map}
+          initialRegion={{
+          latitude: -33.860664,
+          longitude: 151.208138,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        />
     </View>
   );
 }
@@ -16,5 +32,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
